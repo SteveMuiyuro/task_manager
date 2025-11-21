799e830c24455a3fb6b4eb89aeb5e014b91268b1
Steve Mwangi <stevemuiyuro@gmail.com>
2025-11-21 17:19:53 +0300

    remove unused imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import AppRouter from './router'
 import './styles/index.css'
-import { ThemeProvider } from './providers/ThemeProvider'
+
 import { ToastProvider } from './providers/ToastProvider'
 
 const queryClient = new QueryClient()
@@ -12,11 +12,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ToastProvider>
-      <ThemeProvider>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={AppRouter} />
-        </QueryClientProvider>
-      </ThemeProvider>
+        </QueryClientProvider>   
     </ToastProvider>
   </React.StrictMode>
 )
